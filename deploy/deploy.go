8ee1bd6b8ec233fd5d6fde6efdf03c3a6f2c43d6
Shaw Vrana <shaw@vranix.com>
2019-02-20 17:16:56 -0800

    Change CheckPod < 0 logic so we don't mess with blue/green code

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -82,10 +82,6 @@ func CheckPods(cs kubernetes.Interface, namespace string, target RolloutTarget, 
 	if err != nil {
 		return false, errors.Wrapf(err, "failed to get pods for target %s", target.Name())
 	}
-	if len(pods) == 0 {
-		glog.V(4).Infof("target %s has 0 pod", target.Name())
-		return true, nil
-	}
 	if len(pods) < int(num) {
 		glog.V(2).Infof("insufficient pods found for target %s: found %d but need %d", target.Name(), len(pods), num)
 		return false, nil
